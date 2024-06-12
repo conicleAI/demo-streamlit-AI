754feaf45d41d99e8780df1830ac740d00166481
Pun <pannathorn@Conicle.com>
2024-06-12 14:56:05 +0700

    [FIX] Remove error

--- chat-with-Conicle-transcript.py
+++ chat-with-Conicle-transcript.py
@@ -16,8 +16,6 @@ def get_image_as_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode("utf-8")
 
-Your primary tasks are to:
-
 def ingest_data():
     download_from_bucket()
 
