013e2a6a2fc5e4eebb36b5a8812b2ada8edc4d40
Pun <pannathorn@Conicle.com>
2024-06-21 16:45:45 +0700

    Revert "[ADD] Context cache for cost saving"
    This reverts commit b33304524de7ce1d0c7fc7ae62eda4bc07f9e1df.

--- utils/llm.py
+++ utils/llm.py
@@ -1,21 +1,15 @@
-import os
-import datetime
 import lancedb
 import vertexai
 import glob
 import yaml
-import google.generativeai as genai
-import streamlit as st
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import LanceDB
 from langchain_community.vectorstores.chroma import Chroma
 from vertexai.generative_models import GenerativeModel, Part
-from google.generativeai import caching
-from google.generativeai import protos
 
 
 def read_configs(location):
@@ -65,59 +59,20 @@ def create_vector_database(location):
     return vector_store
 
 
-def get_conversational_chain(context, prompt, credentials, setting_location):
-    generation_config = {
-        "temperature": 1,
-        "top_p": 0.95,
-        "top_k": 64,
-        "max_output_tokens": 8192,
-        "response_mime_type": "text/plain",
-    }
-
-    genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+def get_conversational_chain(prompt, credentials, setting_location):
+    vertexai.init(project='conicle-ai', credentials=credentials)
     system_instruction = read_configs(setting_location)['system_instruction']
 
-    context_path = "temp.txt"
-    string_context = [context[i].page_content for i in range(len(context))]
-    string_context = '<doc end>'.join(string_context)
-    with open(context_path, "w") as text_file:
-        text_file.write(string_context)
-    contents = genai.upload_file(path=context_path)
+    model = GenerativeModel(model_name="gemini-1.5-flash",
+                            system_instruction=system_instruction)
 
-    while contents.state.name == "PROCESSING":
-        print('Waiting for contents to be processed.')
-        contents = genai.get_file(contents.name)  
-
-    try:
-        cache = caching.CachedContent.create(
-            model="models/gemini-1.5-flash-001",
-            display_name="related contents",  # used to identify the cache
-            system_instruction=system_instruction,
-            contents=[contents],
-            ttl=datetime.timedelta(minutes=5),
-        )
-
-        model = genai.GenerativeModel.from_cached_content(cached_content=cache, generation_config=generation_config)
-
-        response = model.generate_content(
-            [prompt]
+    response = model.generate_content(
+        [prompt]
     )
 
-    except:
-        prompt = prompt + string_context
-        model = genai.GenerativeModel(
-            model_name="gemini-1.5-flash",
-            generation_config=generation_config
-        )
-
-        response = model.generate_content(
-            [prompt]
-        )
-
-    print(response)
     try:
         result = response.text
     except:
-        result = response.candidates[0].content.parts[0].text
+        result = response.candidates[0].content.parts[0]._raw_part.text
 
     return result
